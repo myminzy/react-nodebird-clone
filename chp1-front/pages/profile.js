6bc6acab0cd055a0615f84b97678813335c8541e
minZzy <120133624+myminzy@users.noreply.github.com>
2025-07-05 17:53:31 +0900

    Add files via upload

---
+++ chp1-front/pages/profile.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Profile = () => {
+    return <div>내 프로필</div>
+};
+
+export default Profile;
+/* 페이지 매핑*/
---
+++ chp1-front/pages/signup.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Signup = () => {
+    return <div>회원가입 페이지</div>
+};
+
+export default Signup;
+/* 페이지 매핑*/
